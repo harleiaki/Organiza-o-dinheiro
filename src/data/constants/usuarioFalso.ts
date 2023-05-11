764aeb174ab1f12acb116f6025c2d2fc5ca19031
HAKI <96266332+harleiaki@users.noreply.github.com>
2023-05-11 13:55:06 -0300

    Add files via upload

---
+++ src/data/constants/usuarioFalso.ts
@@ -0,0 +1,9 @@
+import Id from "@/logic/core/comum/Id";
+import Usuario from "@/logic/core/usuario/Usuario";
+
+export default {
+    id: Id.novo(),
+    nome: 'H Aki',
+    email: 'Haki@xmail.com',
+    imagemUrl: null,
+} as Usuario
